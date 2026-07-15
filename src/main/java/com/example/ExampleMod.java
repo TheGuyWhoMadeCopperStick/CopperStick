ef584e8ec0db72e28b478ac6b2c031e81a01c9d7
TheGuyWhoMadeCopperStick <err504.null@gmail.com>
2026-07-15 16:10:30 -0600

    add copper stick

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,30 +1,28 @@
 package com.example;
 
 import net.fabricmc.api.ModInitializer;
-
-import net.minecraft.resources.ResourceLocation;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
+    public static final String MOD_ID = "copperstick"; 
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+    public static final Item COPPER_STICK = new Item(new Item.Settings());
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+    @Override
+    public void onInitialize() {
+        Registry.register(
+            Registries.ITEM, 
+            Identifier.of(MOD_ID, "copper_stick"), 
+            COPPER_STICK
+        );
 
-		LOGGER.info("Hello Fabric world!");
-	}
-
-	public static ResourceLocation id(String path) {
-		return new ResourceLocation(MOD_ID, path);
-	}
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(COPPER_STICK);
+        });
+    }
 }
